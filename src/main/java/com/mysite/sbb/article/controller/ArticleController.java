f98f0b83535193df7e5717dbcd59ad70b6388d21
chuchu02 <rlaxogns73@gmail.com>
2022-07-12 09:45:32 +0900

    게시물 삭제 및 수정

--- src/main/java/com/mysite/sbb/article/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -5,19 +5,57 @@ import com.mysite.sbb.article.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/usr/article/list")
+@RequestMapping("/usr/article")
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
-    @RequestMapping("")
+    @RequestMapping("/test")
     @ResponseBody
-    public List<Article> articles() {
+    public String testFunc() {
+        return "test";
+    }
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<Article> showList() {
         return articleRepository.findAll();
+    }
+
+    @RequestMapping("/detail")
+    @ResponseBody
+    public Article showArticleDetail(@RequestParam long id) {
+        Optional<Article> article = articleRepository.findById(id);
+        return article.orElse(null);
+    }
+
+    @RequestMapping("/doModify")
+    @ResponseBody
+    public Article doModify(long id, String title, String body) {
+        Article article = articleRepository.findById(id).get();
+        if (title != null) {
+            article.setTitle(title);
+        }
+
+        if (body != null) {
+            article.setBody(body);
+        }
+
+        articleRepository.save(article);
+        return article;
+    }
+
+    @RequestMapping("/doDelete")
+    @ResponseBody
+    public String doDelete(long id) {
+        articleRepository.deleteById(id);
+        return "%d번 게시물이 삭제되었습니다.".formatted(id);
     }
 }
