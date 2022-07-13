49b907bc6769481580f819633c81c4257f541999
chuchu02 <rlaxogns73@gmail.com>
2022-07-13 12:26:19 +0900

    질문목록 과 템플릿

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -2,23 +2,25 @@ package com.mysite.sbb.question.controller;
 
 import com.mysite.sbb.question.dao.QuestionRepository;
 import com.mysite.sbb.question.domain.Question;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/question")
+@AllArgsConstructor
 public class QuestionController {
-    @Autowired
-    private QuestionRepository questionRepository ;
+
+    private final QuestionRepository questionRepository;
 
     @RequestMapping("/list")
-    @ResponseBody
-    public List<Question> showQuestion() {
-        return questionRepository.findAll();
+    public String showQuestion(Model model) {
+        List<Question> questionList =  this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 }
 
