289700910e0b31c717e49cbe5f7a73858dd62bbd
chuchu02 <rlaxogns73@gmail.com>
2022-07-14 11:33:13 +0900

    질문 상세

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,6 +24,14 @@ public class QuestionController {
         List<Question> questionList =  questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+
+        return "question_detail";
     }
 }
 
