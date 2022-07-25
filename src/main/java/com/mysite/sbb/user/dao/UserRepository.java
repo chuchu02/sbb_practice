83404ef2ceed8a5973f65b4185bc0f254284973d
chuchu02 <rlaxogns73@gmail.com>
2022-07-26 01:44:25 +0900

    스프링 시큐리티 로그인/로그아웃

--- src/main/java/com/mysite/sbb/user/dao/UserRepository.java
+++ src/main/java/com/mysite/sbb/user/dao/UserRepository.java
@@ -3,5 +3,8 @@ package com.mysite.sbb.user.dao;
 import com.mysite.sbb.user.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
